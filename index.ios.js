03c78c290e0cb1a65ab1d3b2b5d87d7b0f02f5fe
Henry Doan <henrydoan96@gmail.com>
2016-11-01 13:20:52 -0600

    Passing props

---
+++ index.ios.js
@@ -0,0 +1,54 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React, { Component } from 'react';
+import {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  View
+} from 'react-native';
+
+class Greeting extends Component {
+  render() {
+    return (
+      <Text>Hello {this.props.name}!</Text>
+    );
+  }
+}
+
+export default class GreetingsReactNative extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Greeting name='Rex' />
+        <Greeting name='Mark' />
+        <Greeting name='Sam' />
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+});
+
+AppRegistry.registerComponent('GreetingsReactNative', () => GreetingsReactNative);
